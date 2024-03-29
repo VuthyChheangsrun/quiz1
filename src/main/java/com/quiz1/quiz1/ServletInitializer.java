8bedd304ea470ca0446e069a7476a9ede1bf602d
VuthyChheangsrun <128031514+VuthyChheangsrun@users.noreply.github.com>
2024-03-29 11:38:52 +0700

    a

---
+++ src/main/java/com/quiz1/quiz1/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.quiz1.quiz1;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Quiz1Application.class);
+	}
+
+}
